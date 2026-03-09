6ece9733bfcbb6306f516300eb212cb29b37c8cc
ratiebareeng <bareengbiz@gmail.com>
2026-03-09 16:35:08 +0200

    feat: add navigation to user profile tile

--- lib/features/shell/presentation/user_profile_tile.dart
+++ lib/features/shell/presentation/user_profile_tile.dart
@@ -1,8 +1,10 @@
 import 'package:agricola_core/agricola_core.dart';
 import 'package:agricola_dashboard/core/providers/language_provider.dart';
 import 'package:agricola_dashboard/features/auth/providers/auth_providers.dart';
+import 'package:agricola_dashboard/routing/route_names.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class UserProfileTile extends ConsumerWidget {
   const UserProfileTile({super.key});
@@ -21,6 +23,7 @@ class UserProfileTile extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
       child: ListTile(
+        onTap: () => context.go(RouteNames.profile),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         leading: CircleAvatar(
           child: Text(
